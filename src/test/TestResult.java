c266a01b121004f590a2acb264443f7db5279107
TheOdie <63706431+TheOdie@users.noreply.github.com>
2020-04-23 19:08:27 +0300

    Add files via upload

--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -1,22 +1,5 @@
-package test;
-import domain.Exercise;
-
-/**
- * Functional test class
- * @author Odie
- */
+package Test;
 
 public class TestResult {
 
-    public static void main(String[] args) {
-
-        int R=158635;
-        if(R>=100000 && R<1000000)
-            {
-            System.out.println("Number "+R+" \r\n" + "\r\n" + "in binary notation: "+Exercise.CalculateBinarySystem(R)+"\n\r\n" + "in octal number system: "+Exercise.CalculateOctalSystem(R)+"\n\r\n" + "in hexadecimal notation: "+Exercise.CalculateHexadecimalSystem(R));
-            }
-        else{
-            System.out.println("The number R must be six digits!");
-            }
-    }
 }
